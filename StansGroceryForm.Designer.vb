caddf7cdf84caa96fb2c03d0310da888ca83e620
Justine Hoffa <justinefarrer@yahoo.com>
2020-11-05 08:58:43 -0700

    Add project files.

---
+++ StansGroceryForm.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class StansGroceryForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TopMenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LookUpItemTextBox = New System.Windows.Forms.TextBox()
+        Me.SelectItemComboBox = New System.Windows.Forms.ComboBox()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.LookUpLabel = New System.Windows.Forms.Label()
+        Me.SelectLabel = New System.Windows.Forms.Label()
+        Me.SearchButton = New System.Windows.Forms.Button()
+        Me.LeftGroupBox = New System.Windows.Forms.GroupBox()
+        Me.DisplayLabel = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TopMenuStrip.SuspendLayout()
+        Me.LeftGroupBox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TopMenuStrip
+        '
+        Me.TopMenuStrip.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.TopMenuStrip.ImageScalingSize = New System.Drawing.Size(32, 32)
+        Me.TopMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileTopMenuItem, Me.HelpTopMenuItem})
+        Me.TopMenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.TopMenuStrip.Name = "TopMenuStrip"
+        Me.TopMenuStrip.Size = New System.Drawing.Size(1250, 40)
+        Me.TopMenuStrip.TabIndex = 0
+        Me.TopMenuStrip.Text = "MenuStrip1"
+        '
+        'FileTopMenuItem
+        '
+        Me.FileTopMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FileTopMenuItem.Name = "FileTopMenuItem"
+        Me.FileTopMenuItem.Size = New System.Drawing.Size(72, 36)
+        Me.FileTopMenuItem.Text = "&File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(186, 44)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'HelpTopMenuItem
+        '
+        Me.HelpTopMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutTopMenuItem})
+        Me.HelpTopMenuItem.Name = "HelpTopMenuItem"
+        Me.HelpTopMenuItem.Size = New System.Drawing.Size(85, 36)
+        Me.HelpTopMenuItem.Text = "Help"
+        '
+        'AboutTopMenuItem
+        '
+        Me.AboutTopMenuItem.Name = "AboutTopMenuItem"
+        Me.AboutTopMenuItem.Size = New System.Drawing.Size(214, 44)
+        Me.AboutTopMenuItem.Text = "About"
+        '
+        'LookUpItemTextBox
+        '
+        Me.LookUpItemTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append
+        Me.LookUpItemTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllSystemSources
+        Me.LookUpItemTextBox.Location = New System.Drawing.Point(164, 88)
+        Me.LookUpItemTextBox.Margin = New System.Windows.Forms.Padding(6)
+        Me.LookUpItemTextBox.Name = "LookUpItemTextBox"
+        Me.LookUpItemTextBox.Size = New System.Drawing.Size(818, 31)
+        Me.LookUpItemTextBox.TabIndex = 1
+        '
+        'SelectItemComboBox
+        '
+        Me.SelectItemComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append
+        Me.SelectItemComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.SelectItemComboBox.FormattingEnabled = True
+        Me.SelectItemComboBox.Location = New System.Drawing.Point(164, 138)
+        Me.SelectItemComboBox.Margin = New System.Windows.Forms.Padding(6)
+        Me.SelectItemComboBox.Name = "SelectItemComboBox"
+        Me.SelectItemComboBox.Size = New System.Drawing.Size(432, 33)
+        Me.SelectItemComboBox.TabIndex = 2
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 25
+        Me.DisplayListBox.Location = New System.Drawing.Point(612, 138)
+        Me.DisplayListBox.Margin = New System.Windows.Forms.Padding(6)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(578, 479)
+        Me.DisplayListBox.TabIndex = 3
+        '
+        'LookUpLabel
+        '
+        Me.LookUpLabel.AutoSize = True
+        Me.LookUpLabel.Location = New System.Drawing.Point(10, 88)
+        Me.LookUpLabel.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.LookUpLabel.Name = "LookUpLabel"
+        Me.LookUpLabel.Size = New System.Drawing.Size(138, 25)
+        Me.LookUpLabel.TabIndex = 4
+        Me.LookUpLabel.Text = "Look Up Item"
+        '
+        'SelectLabel
+        '
+        Me.SelectLabel.AutoSize = True
+        Me.SelectLabel.Location = New System.Drawing.Point(32, 138)
+        Me.SelectLabel.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.SelectLabel.Name = "SelectLabel"
+        Me.SelectLabel.Size = New System.Drawing.Size(118, 25)
+        Me.SelectLabel.TabIndex = 5
+        Me.SelectLabel.Text = "Select Item"
+        '
+        'SearchButton
+        '
+        Me.SearchButton.Location = New System.Drawing.Point(998, 88)
+        Me.SearchButton.Margin = New System.Windows.Forms.Padding(6)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(196, 38)
+        Me.SearchButton.TabIndex = 6
+        Me.SearchButton.Text = "&Search"
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
+        'LeftGroupBox
+        '
+        Me.LeftGroupBox.Controls.Add(Me.DisplayLabel)
+        Me.LeftGroupBox.Location = New System.Drawing.Point(164, 190)
+        Me.LeftGroupBox.Margin = New System.Windows.Forms.Padding(6)
+        Me.LeftGroupBox.Name = "LeftGroupBox"
+        Me.LeftGroupBox.Padding = New System.Windows.Forms.Padding(6)
+        Me.LeftGroupBox.Size = New System.Drawing.Size(436, 431)
+        Me.LeftGroupBox.TabIndex = 7
+        Me.LeftGroupBox.TabStop = False
+        '
+        'DisplayLabel
+        '
+        Me.DisplayLabel.Location = New System.Drawing.Point(6, 31)
+        Me.DisplayLabel.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.DisplayLabel.Name = "DisplayLabel"
+        Me.DisplayLabel.Size = New System.Drawing.Size(418, 394)
+        Me.DisplayLabel.TabIndex = 0
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 3000
+        '
+        'StansGroceryForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1250, 667)
+        Me.Controls.Add(Me.LeftGroupBox)
+        Me.Controls.Add(Me.SearchButton)
+        Me.Controls.Add(Me.SelectLabel)
+        Me.Controls.Add(Me.LookUpLabel)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Controls.Add(Me.SelectItemComboBox)
+        Me.Controls.Add(Me.LookUpItemTextBox)
+        Me.Controls.Add(Me.TopMenuStrip)
+        Me.MainMenuStrip = Me.TopMenuStrip
+        Me.Margin = New System.Windows.Forms.Padding(6)
+        Me.Name = "StansGroceryForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Welcome to Stan's Grocery"
+        Me.TopMenuStrip.ResumeLayout(False)
+        Me.TopMenuStrip.PerformLayout()
+        Me.LeftGroupBox.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TopMenuStrip As MenuStrip
+    Friend WithEvents FileTopMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpTopMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutTopMenuItem As ToolStripMenuItem
+    Friend WithEvents LookUpItemTextBox As TextBox
+    Friend WithEvents SelectItemComboBox As ComboBox
+    Friend WithEvents DisplayListBox As ListBox
+    Friend WithEvents LookUpLabel As Label
+    Friend WithEvents SelectLabel As Label
+    Friend WithEvents SearchButton As Button
+    Friend WithEvents LeftGroupBox As GroupBox
+    Friend WithEvents DisplayLabel As Label
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ StansGroceryForm.vb
@@ -0,0 +1,29 @@
+﻿'Justine Hoffa 
+'RCET0265
+'Fall2020
+'StansGrocery
+'
+
+Option Strict On
+Option Explicit On
+Option Compare Binary
+
+Public Class StansGroceryForm
+
+    Private Sub StansGroceryForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim file As String = My.Resources.StansGrocery
+
+        Timer1.Start()
+        SplashScreenForm.BackgroundImageLayout = ImageLayout.Stretch
+        SplashScreenForm.BackgroundImage = My.Resources.stans
+        SplashScreenForm.Size = Me.Size
+        SplashScreenForm.Show()
+        Me.Show()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Stop()
+        SplashScreenForm.Hide()
+    End Sub
+End Class
