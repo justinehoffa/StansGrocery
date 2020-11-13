a53f467b27022495f2c0096bf3daa665dc758a69
Justine Hoffa <justinefarrer@yahoo.com>
2020-11-13 11:16:47 -0700

    pp

--- StansGroceryForm.vb
+++ StansGroceryForm.vb
@@ -12,7 +12,7 @@ Imports System.Text.RegularExpressions
 
 Public Class StansGroceryForm
 
-    Dim array5(255, 2) As String
+    Dim array4(255, 2) As String
 
     Private Sub StansGroceryForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim file As String = My.Resources.StansGrocery
@@ -25,66 +25,65 @@ Public Class StansGroceryForm
         Me.Show()
 
         Dim string1, string2, string3 As String
-        Dim array1(), array2(), array3() As String
+        Dim array1(), array2() As String
         Dim match1 As Match
         Dim integer1, integer2, integer3, integer4 As Integer
 
         string1 = Regex.Replace(My.Resources.StansGrocery, "/", "✷")
-        array1 = Regex.Split(string1, "\p{P}|\p{Sc}")
-        string2 = String.Join("", array1)
-        array2 = Regex.Split(string2, vbLf)
-        Array.Sort(array2)
-        string3 = String.Join("", array2)
-        array3 = Regex.Split(string3, "(?=ITM)|(?=LOC)|(?=CAT)")
+        string2 = Regex.Replace(string1, "\p{P}|\p{Sc}", "")
+        array1 = Regex.Split(string2, vbLf)
+        Array.Sort(array1)
+        string3 = String.Join("", array1)
+        array2 = Regex.Split(string3, "(?=ITM)|(?=CAT)|(?=LOC)")
 
-        For i = 0 To UBound(array3)
-            match1 = Regex.Match(array3(i), "ITM")
+        For i = 0 To UBound(array2)
+            match1 = Regex.Match(array2(i), "ITM")
             If match1.Success = True Then
                 integer1 += 1
             End If
         Next
 
-        Dim array4(integer1 - 1, 2) As String
+        Dim array3(integer1 - 1, 2) As String
         integer2 = 0
 
-        For i = 0 To UBound(array3)
-            match1 = Regex.Match(array3(i), "ITM")
+        For i = 0 To UBound(array2)
+            match1 = Regex.Match(array2(i), "ITM")
             If match1.Success = True Then
-                array4(integer2, 0) = array3(i)
+                array3(integer2, 0) = array2(i)
                 integer2 += 1
             End If
         Next
 
-        For i = 0 To UBound(array3)
-            match1 = Regex.Match(array3(i), "LOC")
+        For i = 0 To UBound(array2)
+            match1 = Regex.Match(array2(i), "LOC")
             If match1.Success = True Then
-                array4(integer3, 1) = array3(i)
+                array3(integer3, 1) = array2(i)
                 integer3 += 1
             End If
         Next
 
-        For i = 0 To UBound(array3)
-            match1 = Regex.Match(array3(i), "CAT")
+        For i = 0 To UBound(array2)
+            match1 = Regex.Match(array2(i), "CAT")
             If match1.Success = True Then
-                array4(integer4, 2) = array3(i)
+                array3(integer4, 2) = array2(i)
                 integer4 += 1
             End If
         Next
 
-        For i = 0 To UBound(array4) - 1
+        For i = 0 To UBound(array3) - 1
             For j = 0 To 2
-                array4(i, j) = Regex.Replace(array4(i, j), "✷", "/")
-                array4(i, j) = Regex.Replace(array4(i, j), "ITM", "")
-                array4(i, j) = Regex.Replace(array4(i, j), "CAT", "")
-                array4(i, j) = Regex.Replace(array4(i, j), "LOC", "")
+                array3(i, j) = Regex.Replace(array3(i, j), "✷", "/")
+                array3(i, j) = Regex.Replace(array3(i, j), "ITM", "")
+                array3(i, j) = Regex.Replace(array3(i, j), "CAT", "")
+                array3(i, j) = Regex.Replace(array3(i, j), "LOC", "")
             Next
         Next
 
-        array5 = array4
+        array4 = array3
 
-        For i = 0 To UBound(array4)
+        For i = 0 To UBound(array3) - 1
             For j = 0 To 2
-                DisplayListBox.Items.Add(array4(i, j))
+                DisplayListBox.Items.Add(array3(i, j))
             Next
         Next
 
